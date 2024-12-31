fb79c6323b0d087d458c769ac6c7d3efcfb3d422
Augustine <mayakiaugustine0@gmail.com>
2024-12-31 12:16:57 +0100

    impelemented data required for tools

--- app/components/sections/Tools/tools.tsx
+++ app/components/sections/Tools/tools.tsx
@@ -1,12 +1,58 @@
-import { tools } from "@/app/constants/constants";
-import Image from "next/image";
+import { ToolType } from "@/app/types/types";
 import React from "react";
-
+import {
+  Css,
+  Html,
+  Javascript,
+  NextJs,
+  ReactLogo,
+  Tailwind,
+  TypeScript,
+} from "../../svg/svg";
 export const Tools = () => {
+  const tools: ToolType[] = [
+    {
+      logo: <Html />,
+      toolName: "HTML",
+      subDescription: "MarkuP Tool",
+    },
+    {
+      logo: <Css />,
+      toolName: "Valina Css",
+      subDescription: "Design Tool",
+    },
+    {
+      logo: <Javascript />,
+      toolName: "JavaScript",
+      subDescription: "Programming language",
+    },
+    {
+      logo: <ReactLogo />,
+      toolName: "React",
+      subDescription: "JavaScript Library",
+    },
+    {
+      logo: <TypeScript />,
+      toolName: "TypeScript",
+      subDescription: "Programming language",
+    },
+    {
+      logo: <Tailwind />,
+      toolName: "Tailwind CSS",
+      subDescription: "CSS Framework",
+    },
+
+    {
+      logo: <NextJs />,
+      toolName: "NextJs",
+      subDescription: "React Framework",
+    },
+  ];
+
   return (
     <div>
-      <div className="  flex items-center 1024:items-start justify-center flex-col gap-[10px] text-center 1024:text-start">
-        <h1 className="text-primary-titleText1 text-[52px] 768:text-[86px] 1300:text-[94px] font-bold leading-[45px] 655:leading-[80px]">
+      <div className="flex items-center 1024:items-start justify-center flex-col gap-[10px] text-center 1024:text-start">
+        <h1 className="text-primary-titleText1 text-[52px] 768:text-[86px] 1300:text-[94px] font-bold leading-[45px] 655:leading-[80px] max-w-[690px] 1024:w-full">
           PREMIUM
           <span className="text-primary-titleText2"> TOOLS</span>
         </h1>
@@ -19,11 +65,7 @@ export const Tools = () => {
             className="flex gap-3 655:gap-5 items-center justify-start p-[16px] rounded-2xl hover:bg-primary-hoverMode hover:!duration-[400ms] hover:transition-colors hover:ease-in-out cursor-default"
           >
             <div className="relative flex items-center justify-center bg-primary-white shadow-lg flex-shrink-0 w-[60px] h-[60px] overflow-hidden rounded-lg">
-              <Image
-                src={tool.logo}
-                alt={tool.alt}
-                className="object-cover object-right w-[40px] h-[40px]"
-              />
+              <div className="w-[40px] h-[40px]">{tool.logo}</div>
             </div>
             <div>
               <p className="text-primary-titleText1 text-base 560:text-lg 768:text-xl 1300:text-[26px] font-bold 1024:font-semibold">
--- app/types/types.ts
+++ app/types/types.ts
@@ -1,5 +1,5 @@
 import { StaticImageData } from "next/image";
-import { ReactElement } from "react";
+import { ReactElement, ReactNode } from "react";
 
 export interface NavItems {
   title: string;
@@ -15,8 +15,7 @@ export interface ProjectType {
 }
 
 export interface ToolType {
-  logo: string | StaticImageData;
-  alt: string;
+  logo: ReactNode;
   toolName: string;
   subDescription: string;
 }
